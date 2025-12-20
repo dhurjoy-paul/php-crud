a0ad4fdfe50522ae1e4698b0f5190aed9f80f90c
Dhurjoy Paul <dev.dhurjoy@gmail.com>
2025-12-20 17:37:53 +0600

    Add: index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  </head>
+  <body>
+    <h1 class="bg-gray-400 py-4 font-medium text-2xl text-center">
+      Simple CRUD with PHP!!!
+    </h1>
+  </body>
+</html>
