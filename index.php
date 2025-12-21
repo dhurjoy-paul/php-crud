9bab0813707e80efb53f46038cc0515f4cac655c
Dhurjoy Paul <dev.dhurjoy@gmail.com>
2025-12-22 00:19:36 +0600

    Show: All data to the table

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
-<?php
-include 'inc/header.php';
-?>
+<?php include 'inc/header.php'; ?>
 
 <p class="flex justify-between items-center bg-gray-200 mx-auto my-8 p-4 rounded-md w-full max-w-5xl font-medium">
   <span class="text-2xl">Student list</span>
   <a href="addStudent.php" class="bg-gray-800 px-4 py-2 rounded-md text-white">Add Student</a>
 </p>
 
-<!-- main table -->
 <table class="mx-auto my-8 p-4 [&_td]:p-2 [&_th]:p-3 rounded-md w-full max-w-5xl text-center">
   <thead>
     <tr>
@@ -20,18 +17,42 @@ include 'inc/header.php';
   </thead>
 
   <tbody class="[&>tr]:odd:bg-gray-200">
-    <tr class="space-x-6 text-center">
-      <td>01</td>
-      <td>Rahim</td>
-      <td>Rahim@dev</td>
-      <td>01234567890</td>
-      <td class='text-white'>
-        <a href=''
-          class='bg-indigo-600 px-3 py-1.5 rounded-md cursor-pointer'>Edit</a>
-        <a href=''
-          class='bg-red-600 px-3 py-1.5 rounded-md cursor-pointer'>Delete</a>
-      </td>
-    </tr>
+    <?php
+    require_once "config/db.php";
+
+    // check connection 
+    if ($connection->connect_error) {
+      die("connection failed: " . $connection->connect_error);
+    }
+
+    // read all row from db table
+    $sql = "SELECT * FROM tbl_students";
+    $result = $connection->query($sql);
+
+    if (!$result) {
+      die("Invalid query: " . $connection->error);
+    }
+
+    // read data of each row
+    while ($row = $result->fetch_assoc()) {
+      echo "
+            <tr>
+              <td>$row[id]</td>
+              <td>$row[name]</td>
+              <td>$row[email]</td>
+              <td>$row[phone]</td>
+              <td class='text-white'>
+                <a 
+                  href='' 
+                  class='bg-indigo-600 px-3 py-1.5 rounded-md cursor-pointer'>Edit</a>
+                <a
+                  href=''
+                  class='bg-red-600 px-3 py-1.5 rounded-md cursor-pointer'>Delete</a>
+              </td>
+            </tr>
+          ";
+    }
+    ?>
   </tbody>
 </table>
 </body>
