0ce43e7cf81177f0e90959281fcd4da2837390e5
Dhurjoy Paul <dev.dhurjoy@gmail.com>
2025-12-23 12:11:21 +0600

    fix: pagination next button bug

--- index.php
+++ index.php
@@ -20,90 +20,90 @@ include 'actions/pagination.php';
 </div>
 
 <!-- main table -->
-<?php
-if ($num > 0) {
-  echo "<table class='mx-auto my-8 p-4 [&_td]:p-2 [&_th]:p-3 rounded-md w-full max-w-5xl text-center'>
-  <thead>
-    <tr class='bg-gray-800 text-white'>
-      <th><input type='checkbox' value=''></th>
-      <th>Serial</th>
-      <th>Name</th>
-      <th>Email</th>
-      <th>Phone</th>
-      <th>Action</th>
-    </tr>
-  </thead>";
-
-  echo "<tbody class='[&>tr]:odd:bg-gray-200'>";
-  while ($row = $result->fetch_assoc()) {
-    echo "
-            <tr>
-              <td>
-                <input type='checkbox' name='checkbox' value='$row[id]'>
-              </td>
-              <td>$row[id]</td>
-              <td>$row[name]</td>
-              <td>$row[email]</td>
-              <td>$row[phone]</td>
-              <td class='text-white'>
-                <a 
-                  href='editStudent.php?id=$row[id]' 
-                  class='bg-indigo-600 px-2 py-1 rounded-md cursor-pointer'>Edit</a>
-                <a
-                  href='actions/deleteStudent.php?id=$row[id]'
-                  class='bg-red-600 px-2 py-1 rounded-md cursor-pointer'>Delete</a>
-              </td>
-            </tr>
-          ";
-  }
-} else {
-  echo " 
-        <p class='py-4 font-medium text-2xl text-center'>
-          No data found!
-        </p>
-        ";
-}
-?>
-</tbody>
-</table>
-
-<!-- pagination -->
 <?php if ($num > 0) { ?>
+  <table class='mx-auto my-8 p-4 [&_td]:p-2 [&_th]:p-3 rounded-md w-full max-w-5xl text-center'>
+    <thead>
+      <tr class='bg-gray-800 text-white'>
+        <th><input type='checkbox' value=''></th>
+        <th>Serial</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>Action</th>
+      </tr>
+    </thead>
 
-  <div class='flex justify-between items-center mx-auto w-full max-w-5xl'>
-    <?php
-    if (!isset($_GET['page_num'])) {
-      $current_page = 1;
-    } else {
-      $current_page = $_GET['page_num'];
-    }
-    ?>
-    <p>Showing <?php echo $current_page; ?> of <?php echo $pages; ?> pages</p>
-    <p class='flex items-center gap-4 font-bold'>
-      <a href='?page_num=1' class='flex justify-center items-center px-1 rounded-md hover:text-blue-800 underline hover:no-underline cursor-pointer'>First</a>
-
-      <?php if (isset($_GET['page_num']) && $_GET['page_num'] > 1) { ?>
-        <a href="?page_num=<?php echo $_GET['page_num'] - 1; ?>"
-          class='flex justify-center items-center bg-gray-600 hover:bg-gray-800 rounded-md w-6 aspect-square text-white cursor-pointer'>-</a>
-      <?php
+    <tbody class='[&>tr]:odd:bg-gray-200'>
+      <?php while ($row = $result->fetch_assoc()) {
+        echo "
+        <tr>
+          <td>
+            <input type='checkbox' name='checkbox' value='$row[id]'>
+          </td>
+          <td>$row[id]</td>
+          <td>$row[name]</td>
+          <td>$row[email]</td>
+          <td>$row[phone]</td>
+          <td class='text-white'>
+            <a
+              href='editStudent.php?id=$row[id]'
+              class='bg-indigo-600 px-2 py-1 rounded-md cursor-pointer'>Edit</a>
+            <a
+              href='actions/deleteStudent.php?id=$row[id]'
+              class='bg-red-600 px-2 py-1 rounded-md cursor-pointer'>Delete</a>
+          </td>
+        </tr>
+        ";
       }
-      for ($i = 1; $i <= $pages; $i++) {
+    } else {
       ?>
-        <a href='?page_num=<?php echo $i; ?>'
-          class='flex justify-center items-center hover:bg-gray-800 border rounded-md w-6 aspect-square hover:text-white cursor-pointer'><?php echo "$i"; ?></a>
-      <?php } ?>
+      <p class='py-4 font-medium text-2xl text-center'>
+        No data found!
+      </p>
+    <?php } ?>
+    </tbody>
+  </table>
+
+  <!-- pagination -->
+  <?php if ($num > 0) { ?>
+
+    <div class='flex justify-between items-center mx-auto w-full max-w-5xl'>
+      <?php
+      if (!isset($_GET['page_num'])) {
+        $current_page = 1;
+      } else {
+        $current_page = $_GET['page_num'];
+      }
+      ?>
+      <p>Showing <?php echo $current_page; ?> of <?php echo $pages; ?> pages</p>
+      <p class='flex items-center gap-4 font-bold'>
+        <a href='?page_num=1' class='flex justify-center items-center px-1 rounded-md hover:text-blue-800 underline hover:no-underline cursor-pointer'>First</a>
+
+        <?php if (isset($_GET['page_num']) && $_GET['page_num'] > 1) { ?>
+          <a href="?page_num=<?php echo $_GET['page_num'] - 1; ?>"
+            class='flex justify-center items-center bg-gray-600 hover:bg-gray-800 rounded-md w-6 aspect-square text-white cursor-pointer'>-</a>
+        <?php
+        }
+        for ($i = 1; $i <= $pages; $i++) {
+        ?>
+          <a href='?page_num=<?php echo $i; ?>'
+            class='flex justify-center items-center hover:bg-gray-800 border rounded-md w-6 aspect-square hover:text-white cursor-pointer'><?php echo "$i"; ?></a>
+        <?php } ?>
 
 
-      <?php if (isset($_GET['page_num']) && $_GET['page_num'] != $pages) { ?>
-        <a href='?page_num=<?php echo $_GET['page_num'] + 1; ?>'
-          class='flex justify-center items-center bg-gray-600 hover:bg-gray-800 rounded-md w-6 aspect-square text-white cursor-pointer'>+</a>
-      <?php } ?>
+        <?php if (isset($_GET['page_num']) && $_GET['page_num'] < $pages) { ?>
+          <a href='?page_num=<?php echo $_GET['page_num'] + 1; ?>'
+            class='flex justify-center items-center bg-gray-600 hover:bg-gray-800 rounded-md w-6 aspect-square text-white cursor-pointer'>+</a>
+        <?php } elseif (!isset($_GET['page_num']) && $pages > 1) { ?>
+          <a href='?page_num=2'
+            class='flex justify-center items-center bg-gray-600 hover:bg-gray-800 rounded-md w-6 aspect-square text-white cursor-pointer'>+</a>
+        <?php } ?>
 
-      <a href='?page_num=<?php echo $pages; ?>' class='flex justify-center items-center px-1 rounded-md hover:text-blue-800 underline hover:no-underline cursor-pointer'>Last</a>
-    </p>
-  </div>
-<?php } ?>
+        <a href='?page_num=<?php echo $pages; ?>' class='flex justify-center items-center px-1 rounded-md hover:text-blue-800 underline hover:no-underline cursor-pointer'>Last</a>
+      </p>
+    </div>
+  <?php } ?>
 
-</body>
+  </body>
 
-</html>
+  </html>
